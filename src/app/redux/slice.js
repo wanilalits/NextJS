d38f2251916fda9ee8654a961eb2e603bca8ce0d
wanilalits <63361672+wanilalits@users.noreply.github.com>
2024-01-12 18:22:14 +0530

    Update slice.js

--- src/app/redux/slice.js
+++ src/app/redux/slice.js
@@ -1,14 +1,11 @@
 const { createSlice, nanoid, current, createAsyncThunk } = require("@reduxjs/toolkit");
-let newusers ; 
-try {
-  newusers = JSON.parse(localStorage.getItem("users")) ? JSON.parse(localStorage.getItem("users")) : []
-} catch (error) {}
+
 
 let initialState = {
-    //users:[] ,//....if no data initially
+  users:[] ,//....if no data initially
   userAPIData: [],
- // users: JSON.parse(localStorage.getItem("users")) ? JSON.parse(localStorage.getItem("users")) : [],
- users:[...newusers]
+  //users: JSON.parse(localStorage.getItem("users")) ? JSON.parse(localStorage.getItem("users")) : [],
+ 
 }
 
 export const fetchApiUsers = createAsyncThunk("fetchApiUsers", async () => {
@@ -53,17 +50,24 @@ console.log(action)
   state.users = data;
     let userdata = JSON.stringify((data));
     localStorage.setItem('users', userdata)
-    }
+    },
+
+    addinitialData: (state, action) => {
+     // console.log(action.payload)
+      state.users=[...action.payload]
+          },
+
   },
   extraReducers: (builder) => {
     builder.addCase(fetchApiUsers.fulfilled, (state, action) => {
       // console.log("reducer call", action);
       state.isloading = false,
         state.userAPIData = action.payload
+      
     })
 
   }
 });
-export const { addUser, removeUser,updateUser } = Slice.actions;
+export const { addUser, removeUser,updateUser,addinitialData } = Slice.actions;
 export default Slice.reducer
 
